7bc741fb165a8f5c7344d41a018d0aa7219a7703
adelorenzo-portainer <81579885+adelorenzo-portainer@users.noreply.github.com>
2022-10-05 07:16:57 -0600

    Create firstboot.sh

---
+++ portainer-api/firstboot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+FLAG="/var/log/firstboot.log"
+if [[ ! -f $FLAG ]]; then
+   #Put here your initialization sentences
+   echo "This is the first boot"
+   PORTAINER_EDGE_ID=$(hostname)
+   podman run -d \
+     -v /run/podman/podman.sock:/var/run/docker.sock \
+     -v /var/lib/containers/storage:/var/lib/docker/volumes \
+     -v /:/host \
+     -v portainer_agent_data:/data \
+     --privileged \
+     --pids-limit=0 \
+     --restart always \
+     -e EDGE=1 \
+     -e EDGE_ID=$PORTAINER_EDGE_ID \
+     -e EDGE_KEY=aHR0cHM6Ly8xOTIuMTY4LjEwLjM6OTQ0M3wxOTIuMTY4LjEwLjM6ODAwMHxjMzpkZDo1ZTpjNDozODo4NzpiZToyYzpmODpjNDo3ODo2YTpmNzo0MTpkYjpjZHww \
+     -e EDGE_INSECURE_POLL=1 \
+     --name portainer_edge_agent \
+     docker.io/portainer/agent:2.15.1
+   #the next line creates an empty file so it won't run the next boot
+   touch "$FLAG"
+else
+   echo "Do nothing"
+fi
